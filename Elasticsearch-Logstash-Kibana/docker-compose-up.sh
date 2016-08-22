0e473f4c98e3e1cbc2b534b834d73c70c6d75125
Alexander Egorov <alexander.egorov@3shape.com>
2016-08-22 13:15:06 +0200

    Matching timestamp for populator logs

--- Elasticsearch-Logstash-Kibana/docker-compose-up.sh
+++ Elasticsearch-Logstash-Kibana/docker-compose-up.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose.yml -f docker-compose.staging.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
 docker-compose scale es-master=5 es-data=4 es-client=3 logstash=3 kibana=3
 docker-compose ps
