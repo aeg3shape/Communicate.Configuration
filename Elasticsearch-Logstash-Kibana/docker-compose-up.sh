72d1ca8647bab711b58e6d0120a16bb3a2cca45d
Alexander Egorov <alexander.egorov@3shape.com>
2016-08-23 11:02:43 +0200

    Updated up script

--- Elasticsearch-Logstash-Kibana/docker-compose-up.sh
+++ Elasticsearch-Logstash-Kibana/docker-compose-up.sh
@@ -1,3 +1,3 @@
 docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
-docker-compose scale es-master=5 es-data=4 es-client=3 logstash=3 kibana=3
-docker-compose ps
+docker-compose -f docker-compose.yml -f docker-compose.production.yml scale es-master=5 es-data=4 es-client=3 logstash=3 kibana=3
+docker-compose -f docker-compose.yml -f docker-compose.production.yml ps
